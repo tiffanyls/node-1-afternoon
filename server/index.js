9271311687cfa35a1b1a3449b5b820ca349dbc6f
Tiffany Schureman <Tiffany@Tiffanys-MacBook-Air.local>
2018-01-31 14:32:41 -0600

    step 5 complete add controller to app

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express=require("express");
 const bodyParser=require("body-parser");
+const messages=require("messages");
 
 const port = 3000;
 
 const app = express();
 
-app.use(json());
+app.use(body.Parser.json());
+
+const { read, create, update, destroy } = require('./controllers/messages_controller');
+
+app.post("/api/messages", create);
+app.get("/api/messages", read);
+app.put("/api/messages/:id", update);
+app.delete("/api/messages/:id", destroy);
 
 app.listen(port, () => {
     console.log("Listening on Port: ${port}");
