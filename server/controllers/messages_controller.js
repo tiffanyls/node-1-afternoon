b5a416ac7392204b261fc5ab97495f0bbc3a70e7
Tiffany Schureman <Tiffany@Tiffanys-MacBook-Air.local>
2018-01-31 14:18:22 -0600

    step 4 message controllers

---
+++ server/controllers/messages_controller.js
@@ -0,0 +1,41 @@
+let messages = [];
+let id = 0;
+
+const create = (req, res, next) => {
+    const {text, time} = req.body;
+    messages.push({id, text, time });
+    id++;
+    res.json(messages);
+};
+
+const read = (req, res, next ) => {
+    res.json(messages);
+};
+
+const update = (req, res, next) => {
+    const {text} = req.body;
+    const updateID = req.params.id;
+    const messageIndex = messages.findIndex (message => message.id === updateID);
+    let message = messages[ messageIndex ];
+    
+    messages[ messageIndex ] = {
+        id: message.id,
+        text: text || message.text,
+        time: message.time
+    };
+    res.json(messages);
+};
+
+const delete = (req, res, next) => {
+    const deleteID = req.params.id;
+    messageIndex = messages.findIndex ( message => message.id === deleteID);
+    messages.splice(messageIndex, 1);
+    res.json(messages);
+};
+
+module.exports = {
+    create,
+    read,
+    udate,
+    destory
+};
